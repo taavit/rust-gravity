baebbe9f048494ceecad1d2e3738818718135e84
Dawid Królak <754920+taavit@users.noreply.github.com>
2019-04-14 00:16:12 +0200

    Detached position from body

--- src/main.rs
+++ src/main.rs
@@ -2,68 +2,92 @@ extern crate image;
 
 use image::{ImageBuffer};
 
+use std::hash::{Hash, Hasher};
+use std::collections::HashMap;
+
 const G: f64 = 6.67*10e-11;
 
-const WIDTH: u32 = 1920;
-const HEIGHT: u32 = 1080;
-
 struct Body {
+    name: String,
     mass: f64,   // Mass in kg
     radius: f64, // radius in meters
-    x: u32,      // position x on plane
-    y: u32,      // position y on plane
+}
+
+impl Hash for Body {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.name.hash(state);
+    }
+}
+
+impl PartialEq for Body {
+    fn eq(&self, other: &Body) -> bool {
+        self.name == other.name
+    }
+}
+impl Eq for Body {}
+
+#[derive(Hash, Eq, PartialEq)]
+struct Position {
+    x: u128,      // position x on plane
+    y: u128,      // position y on plane
+}
+
+struct Plane {
+    bodies: HashMap<Body, Position>,
+    width: usize,
+    height: usize,
 }
 
 fn main() {
-    let mut image : image::RgbImage = ImageBuffer::new(WIDTH, HEIGHT);
-
-    let mut _distance: f64 = 0.0;
-    let mut _gravity: f64 = 0.0;
-    let mut _bright: u8 = 0;
-
-    let mut _m_distance: f64 = 0.0;
-    let mut _m_gravity: f64 = 0.0;
-    let mut _m_bright: u8 = 0;
-
     let mut _moon_gravity: u8 = 0;
 
+    let mut plane = Plane {
+        width: 1920,
+        height: 1080,
+        bodies: HashMap::new(),
+    };
+
     let earth = Body {
+        name: String::from("earth"),
         mass: 5.972 * 10e24,
         radius: 6371.0 * 10e3,
-        x: WIDTH / 3,
-        y: HEIGHT / 2,
     };
 
     let _moon_earth_distance: f64 = 384402.0 * 10e3;
     let _meters_per_pixel: f64 = earth.radius / 12.0;
 
+    plane.bodies.insert(
+        earth,
+        Position {x: plane.width as u128 / 3, y: plane.height as u128 / 2}
+    );
+
     let moon = Body {
+        name: String::from("moon"),
         mass: 7.348 * 10e22,
         radius: 1737.5 * 10e3,
-        x: ((_moon_earth_distance) / _meters_per_pixel) as u32 + earth.x,
-        y: earth.y
     };
 
-    for y in 0..HEIGHT {
-        for x in 0..WIDTH {
-            _distance = distance_to_body(x, y, _meters_per_pixel, &earth);
-            _gravity = (G * earth.mass) / _distance.powi(2);
-            _bright = (clamp(0.0, 255.0, (_gravity * 100000.0) / 255.0)) as u8;
+    plane.bodies.insert(
+        moon, Position {x: plane.width as u128 * 2/3, y: plane.height as u128 / 2}
+    );
 
-            _m_distance = distance_to_body(x, y, _meters_per_pixel, &moon);
-            _m_gravity = (G * moon.mass) / _m_distance.powi(2);
-            _m_bright = (clamp(0.0, 255.0, (_m_gravity * 100000.0) / 255.0)) as u8;
+    let mut image : image::RgbImage = ImageBuffer::new(plane.width as u32, plane.height as u32);
 
-            if _m_gravity > _gravity {
-                _moon_gravity = 255;
-            } else {
-                _moon_gravity = 0;
+    for y in 0..plane.height {
+        for x in 0..plane.width {
+            let mut _distance: f64 = 0.0;
+            let mut _gravity: f64 = 0.0;
+            let mut _bright: u8 = 0;
+            for (key, val) in plane.bodies.iter() {
+                _distance = distance_to_body(x as u32, y as u32, _meters_per_pixel, &val);
+                _gravity = (G * key.mass) / _distance.powi(2);
+                _bright = (clamp(0.0, 255.0, _bright as f64 + (_gravity * 10000000.0) / 255.0)) as u8;
             }
 
-            image.get_pixel_mut(x, y).data = [
+            image.get_pixel_mut(x as u32, y as u32).data = [
                 _bright,
-                _m_bright,
-                _moon_gravity,
+                _bright,
+                _bright,
             ];
         }
     }
@@ -80,9 +104,9 @@ fn clamp(low: f64, high: f64, value: f64) -> f64 {
     return value;
 }
 
-fn distance_to_body(x: u32, y: u32, meters_per_pixel: f64, body: &Body) -> f64 {
-    let _diff_x: i32 = (body.x as i32) - (x as i32);
-    let _diff_y: i32 = (body.y as i32) - (y as i32);
+fn distance_to_body(x: u32, y: u32, meters_per_pixel: f64, position: &Position) -> f64 {
+    let _diff_x: i32 = (position.x as i32) - (x as i32);
+    let _diff_y: i32 = (position.y as i32) - (y as i32);
 
     ((_diff_y as f64 * meters_per_pixel).powi(2) + (_diff_x as f64 * meters_per_pixel).powi(2)).sqrt()
 }
